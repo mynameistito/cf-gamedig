892143a03128ec0601161e0ec2a0ee713f5b499c
Tito <github@mynameistito.com>
2026-08-20 11:10:42 +1200

    Bound fragmented POST request bodies

--- src/container/request-limits.ts
+++ src/container/request-limits.ts
@@ -1,4 +1,5 @@
 export const MAX_POST_BODY_BYTES = 16 * 1024;
+export const MAX_POST_BODY_CHUNKS = 256;
 export const MAX_HOST_LENGTH = 253;
 export const MAX_ADDRESS_LENGTH = 253;
 export const MAX_TYPE_LENGTH = 128;
